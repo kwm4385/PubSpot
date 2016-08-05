5e8f6fac9f25e13cb026c3a3a72f4b42229c8770
Kevin Moses <kmoses@hubspot.com>
2016-08-04 21:28:15 -0400

    Fix config:

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  MONGODB_URI: process.env.DB_PASS || `mongodb://localhost/local`,
+  MONGODB_URI: process.env.MONGODB_URI || `mongodb://localhost/local`,
   BREWERYDB_KEY: process.env.BREWERYDB_KEY
 }
