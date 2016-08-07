730822e7ff62159d748b766501cc120613d43a24
Kevin Moses <kmoses@hubspot.com>
2016-08-06 22:46:01 -0400

    Info list

--- js/components/BeerModal.js
+++ js/components/BeerModal.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import { Dialog, FlatButton } from 'material-ui';
+import { Dialog, FlatButton, Avatar } from 'material-ui';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import { List, ListItem } from 'material-ui/List';
 
 export default class BeerModal extends Component {
 
@@ -18,6 +20,34 @@ export default class BeerModal extends Component {
     this.setState({open: false});
   };
 
+  renderInfo() {
+    const brewery = this.props.data.breweries && _.first(this.props.data.breweries);
+
+    return (
+      <Card>
+        <CardHeader
+          title={this.props.data.nameDisplay}
+          subtitle={this.props.data.breweries && _.first(this.props.data.breweries).name}
+          avatar={this.props.data.labels && this.props.data.labels.icon}
+        />
+        <CardText>
+          {this.props.data.description && <span className="header-text">{this.props.data.description}</span>}
+          <List className="tight">
+            <ListItem
+              primaryText={this.props.data.style.name}
+              secondaryText={this.props.data.style.description}
+            />
+            <ListItem
+              primaryText={brewery.name}
+              secondaryText={brewery.description}
+              leftAvatar={brewery.images && <Avatar src={brewery.images.icon} />}
+            />
+          </List>
+        </CardText>
+      </Card>
+    );
+  }
+
   render() {
     const actions = [
       <FlatButton
@@ -31,14 +61,14 @@ export default class BeerModal extends Component {
       <div>
         <FlatButton label="More..." primary={true} onClick={() => this.handleOpen()} />
         <Dialog
-          title={this.props.data.nameDisplay}
+          bodyStyle={{padding: 0}}
           actions={actions}
           modal={false}
           open={this.state.open}
           autoScrollBodyContent={true}
           onRequestClose={() => this.handleClose()}
         >
-        fdsf
+          {this.renderInfo()}
         </Dialog>
       </div>
     );
