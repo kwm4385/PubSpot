86aecd25b9a52d33c4ca3af0821e224d8077dbe1
Kevin Moses <kmoses@hubspot.com>
2016-08-06 23:16:16 -0400

    tweak

--- js/components/BeerModal.js
+++ js/components/BeerModal.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Dialog, FlatButton, Avatar } from 'material-ui';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 import { List, ListItem } from 'material-ui/List';
 
 export default class BeerModal extends Component {
@@ -20,6 +21,29 @@ export default class BeerModal extends Component {
     this.setState({open: false});
   };
 
+  renderTable(brewery) {
+    const location = brewery && brewery.locations && _.first(brewery.locations);
+
+    return (
+      <Table>
+        <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+          <TableRow>
+            <TableHeaderColumn>ABV</TableHeaderColumn>
+            <TableHeaderColumn>IBU</TableHeaderColumn>
+            <TableHeaderColumn>Origin</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+          <TableRow>
+            <TableRowColumn>{this.props.data.abv}%</TableRowColumn>
+            <TableRowColumn>{this.props.data.ibu ? this.props.data.ibu : '-'}</TableRowColumn>
+            <TableRowColumn>{location ? `${location.locality}, ${location.region}` : '-'}</TableRowColumn>
+          </TableRow>
+        </TableBody>
+      </Table>
+    );
+  }
+
   renderInfo() {
     const brewery = this.props.data.breweries && _.first(this.props.data.breweries);
 
@@ -32,6 +56,7 @@ export default class BeerModal extends Component {
         />
         <CardText>
           {this.props.data.description && <span className="header-text">{this.props.data.description}</span>}
+          {this.renderTable(brewery)}
           <List className="tight">
             <ListItem
               primaryText={this.props.data.style.name}
